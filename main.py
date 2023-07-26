6ac2051cbff2d5a7a0ace3806001420c3e683345
Jass <jasschouhan1010@gmail.com>
2023-07-26 20:38:01 +0530

    added program to get user details

---
+++ main.py
@@ -0,0 +1,10 @@
+# Exide Inverter Selector
+"""This is a program which helps you to select the right inverter and battery for your home and office."""
+# Getting user details
+print("Welcome to Exide Inverter Selector")
+name = input("Enter your name: \n")
+print("Hello", name, "!")
+mob_no = input("Enter your mobile number: \n")
+print("Thank you for providing your details", name, "!")   
+
+
